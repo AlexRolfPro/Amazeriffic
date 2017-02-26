93a4078ba4357f12c34179733feee75e27e6585c
AlexRolfPro <prozebatube@gmail.com>
2017-02-26 19:22:52 +0200

    add new short version code app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
+// var main = function () {
+//  "use strict";
+//  $(".comment__button").on("click", function (event) { //mouse click
+//    var $new_comment;
+//    if($(".comment__input-text").val() !== "") { //input not empty
+//      $new_comment = $("<p>").text($(".comment__input-text").val()); //get input comment
+//      $new_comment.hide(); //hide to slow appear
+//      $(".comment__comments").append($new_comment); //add comment to page
+//      $new_comment.fadeIn(); //show to slow appear
+//      $(".comment__input-text").val(""); //clear input area
+//    }
+//   });
+//   $(".comment__input-text").on("keypress", function (event) { // press "Enter"
+//     var $new_comment;
+//     if( event.keyCode === 13) {
+//       if($(".comment__input-text").val() !== "") { //input not empty
+//      $new_comment = $("<p>").text($(".comment__input-text").val()); //get input comment
+//      $new_comment.hide(); //hide to slow appear
+//      $(".comment__comments").append($new_comment); //add comment to page
+//      $new_comment.fadeIn(); //show to slow appear
+//      $(".comment__input-text").val(""); //clear input area
+//       }
+//     };
+//   });
+// };
+// $(document).ready(main);
+
 var main = function () {
  "use strict";
- $(".comment__button").on("click", function (event) { //mouse click
+ var addCommentFromInputBox = function () { //mouse click
    var $new_comment;
    if($(".comment__input-text").val() !== "") { //input not empty
      $new_comment = $("<p>").text($(".comment__input-text").val()); //get input comment
@@ -9,18 +36,14 @@ var main = function () {
      $new_comment.fadeIn(); //show to slow appear
      $(".comment__input-text").val(""); //clear input area
    }
-  });
-  $(".comment__input-text").on("keypress", function (event) { // press "Enter"
-    var $new_comment;
-    if( event.keyCode === 13) {
-      if($(".comment__input-text").val() !== "") { //input not empty
-     $new_comment = $("<p>").text($(".comment__input-text").val()); //get input comment
-     $new_comment.hide(); //hide to slow appear
-     $(".comment__comments").append($new_comment); //add comment to page
-     $new_comment.fadeIn(); //show to slow appear
-     $(".comment__input-text").val(""); //clear input area
-      }
-    };
-  });
-};
-$(document).ready(main);
+  };
+    $(".comment__button").on("click", function (event) { //mouse click
+     addCommentFromInputBox();
+     });
+    $(".comment__input-text").on("keypress", function (event) { // press "Enter"
+     if (event.keyCode === 13) {
+     addCommentFromInputBox();
+     }
+   });
+ };
+ $(document).ready(main);
