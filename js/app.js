8443a6e17a9652a5905c717b87ccdeef3bfc8321
AlexRolfPro <prozebatube@gmail.com>
2017-02-26 16:42:36 +0200

    Add if check for not empty area in app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 var main = function () {
  "use strict";
  $(".comment__button").on("click", function (event) {
-   var $new_comment = $("<p>").text($(".comment__input-text").val());
-   $(".comment__comments").append($new_comment);
+   var $new_comment;
+   if($(".comment__input-text").val() !== "") {
+     $new_comment = $("<p>").text($(".comment__input-text").val());
+     $(".comment__comments").append($new_comment);
+   }
  });
 };
 $(document).ready(main);
